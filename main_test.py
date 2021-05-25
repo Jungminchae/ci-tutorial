ccc04f4afc60addfe7d866fbe1d2ea703da4fd21
Jungminchae <minchae3618@gmail.com>
2021-05-25 12:08:50 +0900

    fix test

--- main_test.py
+++ main_test.py
@@ -5,7 +5,7 @@ import main
 class MainTest(unittest.TestCase):
     def test_hello_world(self):
         ret = main.hello_world("Test")
-        self.assertEqual(ret, "Hello World! Minchae!")
+        self.assertEqual(ret, "Hello World! Test")
 
 if __name__ == "__main__":
     unittest.main()
